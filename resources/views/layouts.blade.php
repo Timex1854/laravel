2ed4284ec3f0dde28936a150117f4f268fb8d2e4
devJunior061 <catalincretu1234@gmail.com>
2023-02-11 16:53:09 +0200

    3

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -8,38 +8,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/style.scss'])
 </head>
-{{-- <body
-style="background-image: url(https://images.wallpapersden.com/image/download/laptop-room-on-the-desk_Z2hnbGyUmZqaraWkpJRnbW1lrWZtZWU.jpg)"
-> --}}
 
-    <nav class="navbar navbar-dark bg-dark fixed-top">
+<body>
+
+    <nav class="navbar navbar-expand-lg fixed-top bg-dark " data-bs-theme="dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="home">LOGO</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Meniu</h5>
-              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="home">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link " href="contact">Contact</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link " aria-current="page" href="about">
-                    About
-                  </a>
 
-                </li>
-              </ul>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item-white">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item-light">
+                <a class="nav-link" href="#">Link</a>
+              </li>
 
-            </div>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
           </div>
         </div>
       </nav>
